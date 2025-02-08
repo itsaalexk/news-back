5a3889c94409ea4a81518ae0d719a7e4e48e3b8e
PrimeTech <hi.primetech@gmail.com>
2025-02-08 10:34:34 +0100

    configurar endpoint para filtrar por query params desde el frontal

--- controllers/getAllNewsConfig.js
+++ controllers/getAllNewsConfig.js
@@ -4,14 +4,25 @@ import { News } from "../models/news.schema.js";
 export const getAllNews = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
+    const query = req.query.q ? req.query.q.trim() : "";
     const limit = 10;
     const skip = (page - 1) * limit;
     const archived = req.query.archived === "true";
 
-    const news = await News.find({ achieved: archived })
-      .skip(skip)
-      .limit(limit);
-    const total = await News.countDocuments({ achieved: archived });
+    let filter = { achieved: archived };
+
+    if (query) {
+      filter = {
+        ...filter,
+        $or: [
+          { title: { $regex: query, $options: "i" } },
+          { content: { $regex: query, $options: "i" } },
+        ],
+      };
+    }
+
+    const news = await News.find(filter).skip(skip).limit(limit);
+    const total = await News.countDocuments(filter);
 
     res.json({ news, total, page, totalPages: Math.ceil(total / limit) });
   } catch (error) {
